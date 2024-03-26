ec683d8e9402e7f02e0c87b08ed1483aed87fbd7
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-26 19:56:24 +0530

    [Ankit] Add : Created the DbContext for the connection to the database

---
+++ Repository_Layer/Entity/UserEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Repository_Layer.Entity
+{
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Role { get; set; } = "user";
+
+    }
+}
