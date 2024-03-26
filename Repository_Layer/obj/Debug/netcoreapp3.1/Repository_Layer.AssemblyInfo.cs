375d3f9b1775fad0d9a21d1625b73cda0f1bd31f
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-26 20:54:17 +0530

    [Ankit] Add : Added the Bcrypt Liberay for hashing the password

--- Repository_Layer/obj/Debug/netcoreapp3.1/Repository_Layer.AssemblyInfo.cs
+++ Repository_Layer/obj/Debug/netcoreapp3.1/Repository_Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository_Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd91cb07f5e6473ef7a59931713412661a1619b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0239d0f7fe60a567284ca63e7d37d6ecb7bcf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository_Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository_Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
