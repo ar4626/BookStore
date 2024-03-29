bc98a86fb4c44328b9ded1323bf6fd704954da15
Ankit Raj <ankitsingh17072001@gmail.com>
2024-03-29 07:54:33 +0530

    [Ankit] Add : Created the BOOK Entity

--- Repository_Layer/Entity/BookEntitiy.cs
+++ Repository_Layer/Entity/BookEntitiy.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Repository_Layer.Entity
 {
-    internal class BookEntitiy
+    public class BookEntitiy
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string BookDesc { get; set; }
+        public string Author { get; set; }
+        public int Amount { get; set; }
+        public int Discount { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        [ForeignKey("BookBy")]
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public virtual UserEntity BookBy { get; set; }
+
+
+
     }
 }
